fe0fa40c733489a87a0b20b829d6535f3b537242
Amarildo <you@example.com>
2024-05-15 15:31:52 +0200

    added ex-3 and printed number from 1 to 100

---
+++ src/org/learning/FizzBuzz.java
@@ -0,0 +1,9 @@
+package org.learning;
+
+public class FizzBuzz {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++){
+            System.out.println(i);
+        }
+    }
+}
