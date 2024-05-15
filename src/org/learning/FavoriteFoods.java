a2dcd6bcd44a255f27588f043ca56a0eb7a3fb25
Amarildo <you@example.com>
2024-05-15 15:11:52 +0200

    added ex2 FavoriteFoods and create array

---
+++ src/org/learning/FavoriteFoods.java
@@ -0,0 +1,8 @@
+package org.learning;
+
+public class FavoriteFoods {
+    public static void main(String[] args) {
+        //array con il cibo preferito
+        String[] favoriteFoods = {"Pizza", "Lasagna", "Sushi", "Gelato"};
+    }
+}
