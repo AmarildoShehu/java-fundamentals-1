194bcdb8a6aa113897b69912a8bd1f095c698350
Amarildo <you@example.com>
2024-05-15 15:51:10 +0200

    created array list of invited

--- src/org/learning/MetGalaSecurity.java
+++ src/org/learning/MetGalaSecurity.java
@@ -3,5 +3,11 @@ package org.learning;
 public class MetGalaSecurity {
     public static void main(String[] args) {
 
+        //lista array di persone
+        String[] guestList = {
+                "Jennifer Lopez", "Zendaya", "Chris Hemsworth", "Bad Bunny",
+                "Cara Delevingne", "Kendall Jenner", "Uma Thurman",
+                "Damiano David", "Luca Guadagnino"
+        };
     }
 }
